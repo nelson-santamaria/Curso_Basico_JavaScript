eb096ae25cde245b14c3357aa9ae31f1d0ecb2f4
nelson_santamaria <nelson.santamaria@live.com>
2021-04-06 22:33:23 -0500

    Archivos trabajados

---
+++ 1-Values.js
@@ -0,0 +1,37 @@
+//Type of Data
+Number 
+Boolean
+String
+undefined
+null
+Object
+Symbol
+
+
+// Estos tipos de valores se les conoce como Valore primitivos
+
+// Valores. Por ejemplo un valor tipo número
+40 
+
+// Cadena de texto
+"Nelson Santamaria" 
+
+// Valor Booleano
+true 
+false
+
+
+
+null // Valores vacios (Empty values), es como un placeholder (espacio reservado para algun valor) de algún valor faltante
+undefined
+
+// Valores no primitivos u objetos
+
+//Array
+[1,2,3,4,5,6]
+
+//Objeto
+{nombre: "Nelson"}
+
+//Función reservada para saber de que tipo es la variable
+typeof 40 // devuelve que es número
